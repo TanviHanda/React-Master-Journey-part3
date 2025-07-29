8f3b95ab135bfaaf4b624137420b8c48e83a0231
TanviHanda <tanvisona2012@gmail.com>
2025-07-29 14:00:01 +0530

    react-hook-form

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Form1 from "./components/Form1"
+
+const App = () => {
+  return (
+    <div>
+      <Form1/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form1.tsx
@@ -0,0 +1,28 @@
+import {useForm, type SubmitHandler} from "react-hook-form"
+interface FormData {
+    name:string;
+    email:string;
+    password:string;
+}
+const Form1 = () => {
+    const {register,
+        handleSubmit,
+        formState: {errors ,isSubmitted},
+} = useForm<FormData>();
+const onSubmit: SubmitHandler = (data) => {
+
+}
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" {...register('name',{required: 'name is required'})}/>
+
+            {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+  )
+}
+
+export default Form1
